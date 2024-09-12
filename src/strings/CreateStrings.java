fd9ebaca210d096d7e020d1621831197a01f7460
Gopi Gorantala <gopishiva001@outlook.com>
2024-09-12 18:12:33 +0530

    strings initial code

---
+++ src/strings/CreateStrings.java
@@ -0,0 +1,19 @@
+package strings;
+
+/*
+ * This class contains snippets to create strings in Java.
+ * @author ggorantala
+ */
+public class CreateStrings {
+    public static void main(String[] args) {
+        // using string literal
+        String greeting = "Hello, world!";
+
+        // using constructor
+        String name = "John Doe";
+
+        char[] charArray = {'H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', '!'};
+        String newGreeting = new String(charArray); // Hello world!
+
+    }
+}
