f370ceefc964e3167eb30806c02755e142134050
Gopi Gorantala <gopishiva001@outlook.com>
2024-09-13 23:16:56 +0530

    string methods

---
+++ src/strings/StringMethods.java
@@ -0,0 +1,13 @@
+package strings;
+
+/*
+ * The String class in Java provides various methods to manipulate and work with strings effectively.
+ *
+ * @author ggorantala
+ */
+public class StringMethods {
+    public static void main(String[] args) {
+        String text = "Hello, World!";
+        int len = text.length();  // len will be 13
+    }
+}
