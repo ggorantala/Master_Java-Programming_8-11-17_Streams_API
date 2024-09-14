7b46402b5e35c2d478d1f67dff4a358c44be46c1
Gopi Gorantala <gopishiva001@outlook.com>
2024-09-14 22:17:23 +0530

    some changes

--- src/strings/StringMethods.java
+++ src/strings/StringMethods.java
@@ -9,5 +9,7 @@ public class StringMethods {
     public static void main(String[] args) {
         String text = "Hello, World!";
         int len = text.length();  // len will be 13
+        // write some code
+
     }
 }
