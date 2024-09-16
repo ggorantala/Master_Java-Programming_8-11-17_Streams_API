207122653410b1bdf48457b3ae0dd0375356c4db
Gopi Gorantala <gopishiva001@outlook.com>
2024-09-16 09:18:18 +0530

    regular expressions and string serach

---
+++ src/dev/ggorantala/strings/operations/RegularExpressions.java
@@ -0,0 +1,40 @@
+package dev.ggorantala.strings.operations;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+ * This code is part of the course "Master Java 8(11/17), along with Functional Programming With Lambdas And Streams API"
+ * find this course on https://www.ggorantala.dev/all-courses/ page.
+ *
+ * Copyright (C) 2024 by Gopi Gorantala, MIT License.
+ *
+ * @author Gopi Gorantala
+ * Date: 16/09/24
+ */
+public class RegularExpressions {
+    public static void main(String[] args) {
+        findDigitsInString(); // finding digits in string
+        extractingPatterns(); // extracting patterns from a string
+    }
+
+    private static void findDigitsInString() {
+        String text = "My phone number is 12345.";
+        Pattern pattern = Pattern.compile("\\d+");  // The regex \\d+ looks for one or more digits
+        Matcher matcher = pattern.matcher(text);
+
+
+        if (matcher.find()) {
+            System.out.println("Found digits: " + matcher.group());  // Outputs: "12345"
+        }
+    }
+    private static void extractingPatterns() {
+        String text = "Contact us at support@example.com or sales@example.org.";
+        Pattern pattern = Pattern.compile("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
+        Matcher matcher = pattern.matcher(text);
+
+        while (matcher.find()) {
+            System.out.println("Found email: " + matcher.group());
+        }
+    }
+}
--- src/dev/ggorantala/strings/operations/SearchStrings.java
+++ src/dev/ggorantala/strings/operations/SearchStrings.java
@@ -11,6 +11,26 @@ package dev.ggorantala.strings.operations;
  */
 public class SearchStrings {
     public static void main(String[] args) {
+        //searching for a character
+        String text = "Hello, World!";
+        int index = text.indexOf('W');  // 7
+        System.out.println(index);  // Outputs: 7
 
+        // searching a substring
+        int index1 = text.indexOf("World");  // 7
+        System.out.println(index1);  // Outputs: 7
+
+        // when search fails
+        int index2 = text.indexOf('X');  // -1
+        System.out.println(index2);  // Outputs: -1
+
+        // using lastIndexOf()
+        int index3 = text.lastIndexOf('H');  // 7
+        System.out.println(index3);  // Outputs: 7
+
+        // find last occurrence of a substring
+        String text4 = "banana";
+        int index4 = text4.lastIndexOf("an");  // 3
+        System.out.println(index4);  // Outputs: 3
     }
 }
